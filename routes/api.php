27335cf3798002165100483eead5f255f63ef7eb
MohammadFahreza <mohammad_fahreza_28rpl@student.smktelkom-mlg.sch.id>
2020-09-08 18:54:16 +0700

    Menambahkan fungsi UPDATE

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,16 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+   });
+   Route::post('/order', 'order@store');
+   Route::get('/order', 'order@show');
+   Route::get('/order/{id}', 'order@detail');
+
+   Route::post('/product', 'product@store');
+   Route::get('/product', 'product@show');
+   Route::get('/product/{id}', 'product@detail');
+
+   Route::get('/customer', 'customer@show');
+   Route::get('/customer/{id}', 'customer@detail');
+   Route::post('/customer', 'customer@store');
+   Route::put('/siswa/{id}', 'customers@update');
